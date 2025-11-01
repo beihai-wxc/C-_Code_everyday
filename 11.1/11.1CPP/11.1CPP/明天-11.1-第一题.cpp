60dded8405f180191c48aac8504b8343dd937137
beihai-wxc <ggbond2938019159@163.com>
2025-11-01 23:19:24 +0800

    11.1CPP1

---
+++ 11.1/11.1CPP/11.1CPP/明天-11.1-第一题.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+	for (int i = 1; i <N; i++) {
+		if (i % 2 == 0) {
+			cout << i << endl;
+		}
+	}
+	/*
+	for (int i = 2; i < N; i += 2) {
+		cout << i << endl;
+	}
+	*/
+	return 0;
+}
